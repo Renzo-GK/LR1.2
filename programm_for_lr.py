6d06a5834ad7e6656c0c2e148cc26d149de429ae
Omaraschab Guseinov <gk.kobayashi.sss@gmail.com>
2025-02-16 17:57:21 +0300

    added age

--- programm_for_lr.py
+++ programm_for_lr.py
@@ -5,3 +5,14 @@ print("Okay, lets do it anything.?")
 name = str(input("What is your name? \n"))
 
 print("Nice to meet you", name, "!")
+
+age = int(input("How old are you? \n"))
+
+try:
+    if age >18:
+        print("Wow, are you student?")
+    if age <18:
+        print("Wow, are you schoolchild?")
+
+except ValueError:
+    print("Woops.. try again please")
