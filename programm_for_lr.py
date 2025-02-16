61c6ba1ad4b870157d0114697b712bdfd473a61e
Omaraschab Guseinov <gk.kobayashi.sss@gmail.com>
2025-02-16 17:50:40 +0300

    acquaint

--- programm_for_lr.py
+++ programm_for_lr.py
@@ -1,1 +1,7 @@
 print("Hello! This is programm for LR1.2 maked by Omaraschab Guseinov")
+
+print("Okay, lets do it anything.?")
+
+name = str(input("What is your name? \n"))
+
+print("Nice to meet you", name, "!")
