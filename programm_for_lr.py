af9e9f8a1cfda3f3662557619c73dbdd825e3bd9
Omaraschab Guseinov <gk.kobayashi.sss@gmail.com>
2025-02-16 18:09:25 +0300

    Are you student?

--- programm_for_lr.py
+++ programm_for_lr.py
@@ -1,6 +1,6 @@
 print("Hello! This is programm for LR1.2 maked by Omaraschab Guseinov")
 
-print("Okay, lets do it anything.?")
+print("Okay, lets do anything")
 
 name = str(input("What is your name? \n"))
 
@@ -10,9 +10,23 @@ age = int(input("How old are you? \n"))
 
 try:
     if age >18:
-        print("Wow, are you student?")
+        def get_yes_no_1(promt):
+            while True:
+                response = input(promt).strip().lower()
+                if response in ["yes", "да"]:
+                    return True
+                elif response in ["no", "нет"]:
+                    return False
+                else:
+                    print("Woops.. try again please")
+        
+        if get_yes_no_1("Wow, are you student? (yes/no): "):
+            print("Good!")
+        else:
+            print("Hmm.. okay")
+
     if age <18:
         print("Wow, are you schoolchild?")
-
+      
 except ValueError:
     print("Woops.. try again please")
